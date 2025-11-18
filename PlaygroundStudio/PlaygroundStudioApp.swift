be116762aa7ce85799a5b2a31aed45d14b61f43a
Octavianus <oktagandajaya@gmail.com>
2025-11-18 08:33:23 +0700

    Initial Commit

---
+++ PlaygroundStudio/PlaygroundStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundStudioApp.swift
+//  PlaygroundStudio
+//
+//  Created by octavianus on 18/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
